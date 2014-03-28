3b2d30543062273e70e01851767291de13b3eb48
grant <grantpeltier93@gmail.com>
2014-03-28 18:16:43 -0500

    working version of project. still needs polishing and comments

--- A4Driver.java
+++ A4Driver.java
@@ -36,7 +36,7 @@ public class A4Driver {
 			}
 			for(String s = inRead.readLine(); s != null; s = inRead.readLine()){
 				if(s.length() < 11){
-					System.out.print("One or more of the words is less than 5 characters in length.\n");
+					System.out.println("One or more of the words is less than 5 characters in length.\n\n");
 				}else{
 				String from = new String(s.substring(0,WORDLENGTH));
 				int off = 5;
@@ -49,13 +49,13 @@ public class A4Driver {
 				ArrayList<String> empty = new ArrayList<String>();
 				ArrayList<String> attempt = new ArrayList<String>();
 				empty.add(from);
-				ArrayList<String> output = MakeLadder(from, to, 0, empty, 0, words, attempt);
-				if(output.contains("noLadder")){
-					System.out.println("There is no word ladder from " + from + " to " + to);
+				ArrayList<String> output = MakeLadder(from, to, 0, empty, 1, words, attempt);
+				if(output.contains("0error")){
+					System.out.println("There is no word ladder from " + from.substring(1,6) + " to " + to);
 				}else{
 					Iterator<String> i = output.iterator();
 					while(i.hasNext()){
-						System.out.println(i.next());
+						System.out.println(i.next().substring(1,6));
 						}
 					}
 				System.out.print("\n\n");
@@ -74,36 +74,63 @@ public class A4Driver {
 
 	}
 	
+	/**
+	 * 
+	 * @param from
+	 * @param to
+	 * @return
+	 */
 	public static String NumDiffLetters(String from, String to){
 		int numDiff = 0;
 		for(int k = 0; k < from.length(); k++){
-			if(from.charAt(k) == to.charAt(k)){
+			if(from.charAt(k) != to.charAt(k)){
 				numDiff++;
 			}
 		}
 		return numDiff + from;
 	}
 
+	/**
+	 * 
+	 * @param fromWord
+	 * @param toWord
+	 * @param lastChange
+	 * @param ladder
+	 * @param marker
+	 * @param words
+	 * @param attempted
+	 * @return
+	 */
 	public static ArrayList<String> MakeLadder(String fromWord, String toWord, int lastChange, ArrayList<String> ladder, int marker, Dictionary words, ArrayList<String> attempted){
-		if(Integer.parseInt(ladder.get(ladder.size()).substring(0,1)) == 0){ return ladder;}
-		marker = (marker+1) % 6;
-		if(marker == 0){marker++;}
-		if(marker == lastChange){			// what about when nothing even works for the first word????
-			ladder.remove(ladder.size());
+		if(Integer.parseInt(ladder.get(ladder.size()-1).substring(0,1)) == 0){ return ladder;}
+		if(Integer.parseInt(ladder.get(ladder.size()-1).substring(0,1)) == 1){
+			fromWord = NumDiffLetters(toWord, toWord);
+			ladder.add(fromWord);
+			return ladder;
+		}
+		if(marker == lastChange){			
+			ladder.remove(ladder.size()-1);
 			if(ladder.isEmpty()){
-				ladder.add("error");
+				ladder.add("0error");
 				return ladder;
+			}else{
+			MakeLadder((ladder.get(ladder.size()-1)), toWord, lastChange, ladder, (marker+1)%6, words, attempted);
+				}
 			}
-			MakeLadder(ladder.get(ladder.size()), toWord, lastChange, ladder, marker, words, attempted);
-			}
-		String newWord = words.findWord(fromWord, marker, attempted);
+		int realChange;
+		if(marker == 0){realChange = marker;
+		}else{realChange = marker - 1; }
+		int numDiff = Integer.parseInt(fromWord.substring(0,1));
+		String newWord = words.findWord(fromWord.substring(1,6), realChange, attempted, numDiff, toWord);
 		if(newWord.equals("noWord")){									// the logic of this if/else is more iterative than recursive
-			MakeLadder(fromWord, toWord, lastChange, ladder, marker, words, attempted); 	// this wont work here. It will just want to return
+			MakeLadder(fromWord, toWord, lastChange, ladder, (marker+1)%6, words, attempted); 	// this wont work here. It will just want to return
+			return ladder;
 		}else{
 			attempted.add(newWord);
 			newWord = NumDiffLetters(newWord, toWord);
 			ladder.add(newWord);
-			lastChange = marker;
+			MakeLadder(newWord, toWord, marker, ladder, (marker+1)%6, words, attempted);
+			return ladder;
 		}
 	}
 	
--- Dictionary.java
+++ Dictionary.java
@@ -5,26 +5,62 @@ import java.util.*;
 public class Dictionary {
 	ArrayList<String> dictionary;
 	
+	/**
+	 * 
+	 */
 	Dictionary(){
 		dictionary = new ArrayList<String>();
 	}
 	
+	/**
+	 * 
+	 * @param word
+	 */
 	public void addWord(String word){
 		dictionary.add(word);
 	}
 	
+	/**
+	 * 
+	 * @param base
+	 * @param newWord
+	 * @param change
+	 * @return
+	 */
 	public boolean equalExcept(String base, String newWord, int change){
 		int k = 0;
 		while(k < base.length()){
 			if(base.charAt(k) != newWord.charAt(k) && k != change){return false;}
+			k++;
 		}
 		return true;
 	}
 	
-	public String findWord(String base, int change, ArrayList<String> attempt){
+	/**
+	 * 
+	 * @param base
+	 * @param change
+	 * @param attempt
+	 * @param numDiff
+	 * @param to
+	 * @return
+	 */
+	public String findWord(String base, int change, ArrayList<String> attempt, int numDiff, String to){
 		Iterator<String> i = dictionary.iterator();
+		ArrayList<String> possible = new ArrayList<String>();
 		while(i.hasNext()){
-			if(equalExcept(base, i.next(), change) && !attempt.contains(i.next())){return i.next();}
+			String x = i.next();
+			if(equalExcept(base, x, change) && !attempt.contains(x)){
+				String word = x;
+				word = A4Driver.NumDiffLetters(word, to);
+				possible.add(word);
+				}
+		}
+		Iterator<String> j = possible.iterator();
+		while(j.hasNext()){
+			String y = j.next();
+			int z = Integer.parseInt(y.substring(0,1));
+			if(z <= numDiff){return y.substring(1,6);}
 		}
 		return "noWord";
 	}
